a96011a47b2798a4bbc843364ecbcf70bca6a758
dikranteymur <mdteymur@gmail.com>
2022-01-26 10:27:26 +0300

    Files added.

---
+++ CoordinatorPatternPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorPatternPOC
+//
+//  Created by Dikran Teymur on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
